8004eec1deb5521dfcf1d974a07f5745ccee5dc7
hiltonfarias <hilton.farias.da.silva@gmail.com>
2020-04-16 22:00:45 -0300

    end stats

---
+++ stats.js
@@ -0,0 +1,19 @@
+const os = require('os')
+
+const { freemem, totalmem } = os
+
+setInterval(() => {
+  const total = parseInt(totalmem() / 1024 / 1024)
+  const men = parseInt(freemem() / 1024 / 1024)
+  const percents = parseInt((1.0 - men / total) * 100)
+
+  const stats = {
+    free: `${men} MB`,
+    total: `${total} MB`,
+    usage: `${percents} %`,
+  }
+
+  console.clear()
+  console.log('====== PC STATS =======')
+  console.table(stats)
+}, 1000)
